6252a25e5d6ada006439fd229f6de782bbca0fe2
sarah-thong <sarah.thong@switcheo.network>
2022-03-30 14:59:39 +0800

    Remove strange indent

--- colors.js
+++ colors.js
@@ -1,7 +1,7 @@
 // To view whole color palette, visit the following webpage
 // https://coolors.co/f72585-b5179e-7209b7-560bad-480ca8-3a0ca3-3f37c9-4361ee-4895ef-4cc9f0
 module.exports = [
-	'#F72585', // flickr pink
+  '#F72585', // flickr pink
   '#B5179E', // byzantine
   '#7209B7', // purple (light)
   '#560BAD', // purple (dark)
