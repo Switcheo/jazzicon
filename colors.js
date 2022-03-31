776fd646e2416dde38143c439c0b27bfb38e57a0
sarah-thong <sarah.thong@switcheo.network>
2022-03-31 18:28:50 +0800

    Change to new color palette

--- colors.js
+++ colors.js
@@ -1,14 +1,9 @@
 // To view whole color palette, visit the following webpage
-// https://coolors.co/f72585-b5179e-7209b7-560bad-480ca8-3a0ca3-3f37c9-4361ee-4895ef-4cc9f0
+// https://coolors.co/5c58b6-b957ce-5994ce-3a4e93-d9e0fc
 module.exports = [
-  '#C577F9', // heliotrope
-  '#A329F5', // purple x11
-  '#7209B7', // purple (light)
-  '#560BAD', // purple (dark)
-  '#480CA8', // trypan blue (light)
-  '#430EBE', // medium blue
-  '#3F37C9', // persian blue
-  '#4361EE', // ultramarine blue
-  '#8FA2F5', // cornflower blue
+  '#5C58B6', // liberty
+  '#B957CE', // medium orchid
+  '#5994CE', // blue gray
+  '#3A4E93', // Y in Mn Blue
   '#D9E0FC', // lavender web
 ]
