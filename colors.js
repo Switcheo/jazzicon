78d85254aefeb5f2f23187d27e4d9e91073947be
sarah-thong <sarah.thong@switcheo.network>
2022-03-31 15:27:13 +0800

    Make colour palette slightly cooler

--- colors.js
+++ colors.js
@@ -1,14 +1,14 @@
 // To view whole color palette, visit the following webpage
 // https://coolors.co/f72585-b5179e-7209b7-560bad-480ca8-3a0ca3-3f37c9-4361ee-4895ef-4cc9f0
 module.exports = [
-  '#F72585', // flickr pink
-  '#B5179E', // byzantine
+  '#C577F9', // heliotrope
+  '#A329F5', // purple x11
   '#7209B7', // purple (light)
   '#560BAD', // purple (dark)
   '#480CA8', // trypan blue (light)
-  '#3A0CA3', // trypan blue (dark)
+  '#430EBE', // medium blue
   '#3F37C9', // persian blue
   '#4361EE', // ultramarine blue
-  '#4895EF', // dodger blue
-  '#4CC9F0', // vivid sky blue
+  '#8FA2F5', // cornflower blue
+  '#D9E0FC', // lavender web
 ]
